ea3a99b15fa24bf35f7cd2a5c9863f8b4073f209
Susie <susie.sko@gmail.com>
2021-02-04 20:09:26 -0800

    after lecture 393

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const indexPage = () => {
+const authIndexPage = ({appName}) => {
   return (
     <div>
-      <h1>The Auth Page</h1>
+      <h1>The Auth Index Page - {appName}</h1>
       <User name="Max" age={28}/>
     </div>
   );
 }
 
-export default indexPage;
+authIndexPage.getInitialProps = context => {
+  console.log(context);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: 'Super App Auth'});
+    }, 1000)
+  });
+  return promise;
+}
+
+
+export default authIndexPage;
